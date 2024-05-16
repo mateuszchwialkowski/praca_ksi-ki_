591bc4b3d1d064bbe4cb31028749049a5d636883
mateuszchwialkowski <152617426+mateuszchwialkowski@users.noreply.github.com>
2024-05-17 00:20:32 +0200

    Add files via upload

---
+++ Ksiazki/Program.cs
@@ -0,0 +1,76 @@
+﻿using Ksiazki;
+
+// Funkcja wyświetlająca menu i obsługująca interakcję użytkownika
+void menu(string? message = "") {
+  Console.Clear(); // Czyści ekran konsoli
+
+  // Jeśli jest przekazana wiadomość, wyświetla ją
+  if(!string.IsNullOrEmpty(message)) Console.WriteLine(message);
+
+  // Wyświetla opcje menu
+  Console.WriteLine("1. Wyswietl wszystkie ksiazki");
+  Console.WriteLine("2. Wyswietl szczegolna ksiazke");
+  Console.WriteLine("3. Dodaj ksiazke");
+  Console.WriteLine("4. Usun ksiazke");
+
+  // Pobiera wybór użytkownika i konwertuje go na liczbę całkowitą
+  int i = Convert.ToInt16(Console.ReadLine());
+  
+  // Obsługa wyboru użytkownika za pomocą instrukcji switch
+  switch (i)
+  {
+    default: 
+      // Jeśli wybór nie pasuje do żadnej z opcji, ponownie wywołuje menu
+      menu();
+      break;
+
+    case 1: 
+      // Wyświetla wszystkie książki
+      Ksiazka.ShowAll();
+      Console.ReadKey(); // Czeka na naciśnięcie klawisza
+
+      menu(); // Ponownie wywołuje menu
+      break;
+
+    case 2:
+      // Wyświetla szczegóły wybranej książki
+      Console.WriteLine("Podaj id ksiazki, ktora chcesz wyswietlic");
+      Ksiazka.Show(Convert.ToInt16(Console.ReadLine()));
+      Console.ReadKey(); // Czeka na naciśnięcie klawisza
+
+      menu(); // Ponownie wywołuje menu
+      break;
+
+    case 3:
+      // Dodaje nową książkę
+      Console.WriteLine("Podaj tytul");
+      string title = Console.ReadLine(); // Pobiera tytuł od użytkownika
+
+      Console.WriteLine("Podaj autora");
+      string author = Console.ReadLine(); // Pobiera autora od użytkownika
+
+      Console.WriteLine("Podaj rok wydania");
+      int releaseDate = Convert.ToInt16(Console.ReadLine()); // Pobiera rok wydania od użytkownika
+
+      Console.WriteLine("Podaj gatunek");
+      string genre = Console.ReadLine(); // Pobiera gatunek od użytkownika
+
+      // Dodaje książkę do listy
+      Ksiazka.Add(title, author, releaseDate, genre);
+
+      // Wywołuje menu z komunikatem o dodaniu książki
+      menu("Ksiazka dodana!");
+      break;
+
+    case 4:
+      // Usuwa książkę
+      Console.WriteLine("Podaj id ksiazki, ktora chcesz usunac");
+      Ksiazka.Remove(Convert.ToInt16(Console.ReadLine())); // Pobiera id książki do usunięcia
+
+      // Wywołuje menu z komunikatem o usunięciu książki
+      menu("Ksiazka usunieta!");
+      break;
+  }
+}
+menu(); 
+// Wywołuje funkcję menu przy starcie programu
---
+++ Ksiazki/ksiazki.cs
@@ -0,0 +1,101 @@
+﻿using System.Text.Json;
+
+namespace Ksiazki
+{
+  public class Ksiazka
+  {
+    // Lista przechowująca obiekty typu Ksiazka, inicjowana na początku programu
+    static List<Ksiazka>? ksiazki = new List<Ksiazka>();
+
+    // Ścieżka do pliku JSON, gdzie są zapisane dane książek
+    static string path = "Ksiazki.json";
+
+    // Konstruktor statyczny, który wywołuje metodę RefreshData, aby zaktualizować listę książek z pliku
+    static Ksiazka() => RefreshData();
+
+    // Metoda odświeżająca dane książek z pliku JSON
+    public static void RefreshData() =>
+      ksiazki = JsonSerializer.Deserialize<List<Ksiazka>>(File.ReadAllText(path));
+
+    // Metoda sprawdzająca, czy książka o danym id istnieje w liście
+    public static bool Exists(int id) {
+      return ksiazki.Any(k => k.Id == id);
+    }
+
+    // Metoda dodająca nową książkę do listy i zapisująca zaktualizowaną listę do pliku JSON
+    public static void Add(string tytul, string autor, int rokWydania, string gatunek)
+    {
+      ksiazki.Add(
+        new Ksiazka {
+            Id = ksiazki.Count + 1, // Przypisanie id jako liczby książek + 1
+            Tytul = tytul,
+            Autor = autor,
+            RokWydania = rokWydania,
+            Gatunek = gatunek,
+          });
+
+      // Zapisanie zaktualizowanej listy książek do pliku JSON
+      File.WriteAllText(path, JsonSerializer.Serialize(ksiazki));
+    }
+
+    // Metoda usuwająca książkę o danym id z listy i zapisująca zmiany do pliku JSON
+    public static void Remove(int id)
+    {
+      if (!Exists(id)) {
+        Console.WriteLine("Ksiazka nie istnieje.");
+        return;
+      }
+
+      // Usunięcie książki z listy
+      ksiazki.RemoveAll((k) => k.Id == id);
+      
+      // Normalizacja id po usunięciu książki
+      NormalizeIds();
+
+      // Zapisanie zaktualizowanej listy książek do pliku JSON
+      File.WriteAllText(path, JsonSerializer.Serialize(ksiazki));
+    }
+
+    // Metoda normalizująca id książek, aby nie było luk w numeracji po usunięciu książki
+    public static void NormalizeIds() =>
+      ksiazki.ForEach(k => k.Id = ksiazki.IndexOf(k) + 1);
+
+    // Metoda wyświetlająca informacje o książce o danym id
+    public static void Show(int id)
+    {
+      if (!Exists(id))
+      {
+        Console.WriteLine("Ksiazka nie istnieje.");
+        return;
+      }
+      Ksiazka k = ksiazki.Find(k => k.Id == id);
+
+      Info(k);
+    }
+
+    // Metoda wyświetlająca informacje o wszystkich książkach
+    public static void ShowAll()
+    {
+      ksiazki.ForEach(k =>
+      {
+        Info(k);
+        Console.WriteLine("---------------------");
+      });
+    }
+
+    // Metoda wyświetlająca szczegółowe informacje o podanej książce
+    public static void Info(Ksiazka k) {
+      Console.WriteLine($"{k.Id} | {k.Tytul}");
+      Console.WriteLine(k.Gatunek);
+      Console.WriteLine(k.Autor);
+      Console.WriteLine(k.RokWydania);
+    }
+
+    // Właściwości klasy Ksiazka
+    public int Id { get; set; }
+    public string? Tytul { get; set; }
+    public string? Autor { get; set; }
+    public int RokWydania { get; set; }
+    public string? Gatunek { get; set; }
+  }
+}
